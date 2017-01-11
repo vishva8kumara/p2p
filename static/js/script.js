b8201e2f7ab5098a3ce3777ef9f24443036c9be1
Vishva Kumara <vishva@villvay.com>
2017-01-11 15:13:36 +0530

    fixes

--- static/js/script.js
+++ static/js/script.js
@@ -1,18 +1,23 @@
 
 new arc.nav('settings', arc.q('#settings')[0],
 	function(context, params){
+		var pre = context.q('pre')[0];
 		new arc.ajax('settings', {
 			callback: function(data){
-				context.q('pre')[0].html(JSON.stringify(data.data, false, 3));
+				pre.html(JSON.stringify(data.data, false, 3));
 			}
 		});
 	});
 
 new arc.nav('users', arc.q('#users')[0],
 	function(context, params){
+		var list = context.q('ul')[0];
+		var data = {};//data.data[user]
 		new arc.ajax('users', {
-			callback: function(data){
-				context.q('pre')[0].html(JSON.stringify(data.data, false, 3));
+			callback: function(res){
+				data = res.data;
+				for (var user in data)
+					list.a(arc.elem('li', user));
 			}
 		});
 	});
